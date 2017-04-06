81a174152b05a3a675632ac9a2a4bf393a3d4d03
Sphaerichthys <sphaerichthys12@gmail.com>
2017-04-06 16:08:09 +0300

    Test

---
+++ TaskManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+
+namespace Lib
+{
+    public class TaskManager
+    {
+        private readonly List<TaskEntry> _list = new List<TaskEntry>();
+        private readonly object _locker = new object();
+
+        public void AddTask(Action task)
+        {
+            lock (_locker)
+                _list.Add(new TaskEntry(task));
+        }
+
+        public void AddTasks(IEnumerable<Action> tasks)
+        {
+            var entries = tasks.ToList().Select(task => new TaskEntry(task));
+            lock (_locker)
+                _list.AddRange(entries);
+        }
+
+        public void InvokeTasks()
+        {
+            for (var i = 0; i < _list.Count; i++)
+            {
+                lock (_locker)
+                {
+                    if (_list[i].IsRunning)
+                    {
+                        continue;
+                    }
+                    _list[i].IsRunning = true;
+                }
+
+                _list[i].Task.Invoke();
+
+                lock (_locker)
+                {
+                    _list[i].IsRunning = false;
+                }
+            }
+        }
+
+        private class TaskEntry
+        {
+            public Action Task { get; private set; }
+
+            public bool IsRunning { get; set; }
+
+            public TaskEntry(Action task)
+            {
+                Task = task;
+                IsRunning = false;
+            }
+        }
+    }
+
+}
