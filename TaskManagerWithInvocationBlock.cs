26cfd06bc7dfe8948472950df654d3a787b098cb
Sphaerichthys <sphaerichthys12@gmail.com>
2017-04-06 16:22:04 +0300

    Block improvements

--- TaskManagerWithInvocationBlock.cs
+++ TaskManagerWithInvocationBlock.cs
@@ -5,17 +5,18 @@ namespace Lib
     public class TaskManagerWithInvocationBlock
     {
         private Action _task;
-        private readonly object _locker = new object();
+        private readonly object _addLocker = new object();
+        private readonly object _invokeLocker = new object();
 
         public void AddTask(Action task)
         {
-            lock (_locker)
+            lock (_addLocker)
                 _task += task;
         }
 
         public void InvokeActions()
         {
-            lock (_locker)
+            lock (_invokeLocker)
                 _task.Invoke();
         }
     }
